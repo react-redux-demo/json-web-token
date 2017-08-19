20de1b49a937b688d44d05741a1a7dcebfb47d84
liyanyan19940914 <921258351@qq.com>
2017-08-19 23:19:28 +0800

    get res is success

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+var User = require('./module/user');
+const config = require('./config');
 
 const app = express();
+
+mongoose.connect(config.database);
+app.set('secret',config.secret);
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/',function (req,res) {
+    res.send('hello world')
+})
 
 app.listen(3000,function () {
     console.log('server started at port:3000')
